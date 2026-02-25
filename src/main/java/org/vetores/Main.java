48c5988842eaae249258717fd41f2a7d39897ad1
Tiago Gomes <tidgomes@gmail.com>
2026-02-25 12:06:02 -0300

    calculate average of product prices

--- src/main/java/org/vetores/Main.java
+++ src/main/java/org/vetores/Main.java
@@ -1,5 +1,7 @@
 package org.vetores;
 
+import org.entities.Product;
+
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -10,22 +12,27 @@ public class Main {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Provide the array size");
+        System.out.println("Provide the size of the vector: ");
         int n = sc.nextInt();
-        double vect[] = new double[n];
+        Product[] products = new Product[n];
 
         for (int i = 0; i < n; i++) {
-            System.out.printf("Provide the value for position %d: %n", i);
-            vect[i] = sc.nextDouble();
+            System.out.printf("Provide the name of product %d: %n", i + 1);
+            String name = sc.next();
+            System.out.printf("Provide the price of product %d: %n", i + 1);
+            double price = sc.nextDouble();
+
+            Product product = new Product(name, price);
+            products[i] = product;
         }
 
         double sum = 0;
         for (int i = 0; i < n; i++) {
-            sum += vect[i];
+            sum += products[i].getPrice();
         }
 
-        double average = sum / n;
-        System.out.printf("The average of heights is: %.2f%n", average);
+        double priceAverage = sum / n;
+        System.out.printf("The average price of the products is: $%.2f%n", priceAverage);
 
         sc.close();
     }
