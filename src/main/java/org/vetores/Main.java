9c5e3ed53b8a29171ca5dbf49a955fc6fda5da7a
Tiago Gomes <tidgomes@gmail.com>
2026-02-25 11:43:06 -0300

    first example with vectors

--- src/main/java/org/vetores/Main.java
+++ src/main/java/org/vetores/Main.java
@@ -1,8 +1,32 @@
 package org.vetores;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Java, how are you doing? Long time no see... glad to see you >.<");
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Provide the array size");
+        int n = sc.nextInt();
+        double vect[] = new double[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.printf("Provide the value for position %d: %n", i);
+            vect[i] = sc.nextDouble();
+        }
+
+        double sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += vect[i];
+        }
+
+        double average = sum / n;
+        System.out.printf("The average of heights is: %.2f%n", average);
+
+        sc.close();
     }
 }
