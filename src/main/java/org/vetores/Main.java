8fc0048d37b67dbb08547caff4acfef98ff6a0d2
Tiago Gomes <tidgomes@gmail.com>
2026-02-25 11:31:37 -0300

    >.<

---
+++ src/main/java/org/vetores/Main.java
@@ -0,0 +1,8 @@
+package org.vetores;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java, how are you doing? Long time no see... glad to see you >.<");
+    }
+}
