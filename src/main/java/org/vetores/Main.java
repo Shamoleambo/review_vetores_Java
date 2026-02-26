d96c50751ccc68e2ea17ef417754464f1a3fea9f
Tiago Gomes <tidgomes@gmail.com>
2026-02-26 09:11:56 -0300

    boxing and unboxing in Java

--- src/main/java/org/vetores/Main.java
+++ src/main/java/org/vetores/Main.java
@@ -1,39 +1,18 @@
 package org.vetores;
 
-import org.entities.Product;
-
-import java.util.Locale;
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        Locale.setDefault(Locale.US);
-        Scanner sc = new Scanner(System.in);
+        int x = 20;
+        Object xa = x;
 
-        System.out.println("Provide the size of the vector: ");
-        int n = sc.nextInt();
-        Product[] products = new Product[n];
+        System.out.println(x);
+        System.out.println(xa);
 
-        for (int i = 0; i < n; i++) {
-            System.out.printf("Provide the name of product %d: %n", i + 1);
-            String name = sc.next();
-            System.out.printf("Provide the price of product %d: %n", i + 1);
-            double price = sc.nextDouble();
-
-            Product product = new Product(name, price);
-            products[i] = product;
-        }
-
-        double sum = 0;
-        for (int i = 0; i < n; i++) {
-            sum += products[i].getPrice();
-        }
-
-        double priceAverage = sum / n;
-        System.out.printf("The average price of the products is: $%.2f%n", priceAverage);
-
-        sc.close();
+        Object ya = 30;
+        int y = (int) ya;
+        System.out.println(y);
+        System.out.println(ya);
     }
 }
