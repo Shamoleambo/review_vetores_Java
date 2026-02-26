5ea1019362a4479b562fe75bdd503a1b96aa4c69
Tiago Gomes <tidgomes@gmail.com>
2026-02-26 09:36:18 -0300

    add elements in lists

--- src/main/java/org/vetores/Main.java
+++ src/main/java/org/vetores/Main.java
@@ -1,21 +1,19 @@
 package org.vetores;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        int[] x = new int[3];
-        x[0] = 1;
-        x[1] = 2;
-        x[2] = 3;
+        List<String> names = new ArrayList<>();
 
-        String[] names = new String[]{"Maria", "Bod", "Alex"};
+        names.add("Maria");
+        names.add("Alex");
+        names.add("Bob");
+        names.add("Ana");
 
-        for (int num : x) {
-            System.out.println(num);
-        }
-
-        System.out.println();
         for (String name : names) {
             System.out.println(name);
         }
